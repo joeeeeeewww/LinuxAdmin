331fe7ab8baf2c7c513ca9e59688508cf332efd6
joe <joewwwh@gmail.com>
2023-07-30 12:00:44 +0800

    acceptdrop

--- filterByDatetime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# this script accepts only traffic from 10am til 4pm
-iptables -F
-
-iptables -A INPUT -p tcp --dport 22 -m time --timestart 10:00 --timestop 6:00 -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j DROP
